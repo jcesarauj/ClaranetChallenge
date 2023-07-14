4f41082f6c7c9018265d8b1a783dcd79d99a43c0
Julio Cesar de Araujo <34680147+jcesarauj@users.noreply.github.com>
2023-07-14 14:07:34 -0300

    Update create-database.sql

--- backend/docker/src/create-database.sql
+++ backend/docker/src/create-database.sql
@@ -8,7 +8,7 @@ CREATE TABLE dbo.NewsLetter(
 	Id int IDENTITY,
 	Email varchar(255) NOT NULL,
 	HeardAboutUs int NOT NULL,
-    ReasonForSigningUp varchar(255) NULL,
+    	ReasonForSigningUp varchar(255) NULL,
 	CONSTRAINT PK_Id PRIMARY KEY (Id),
 	CONSTRAINT AK_Email UNIQUE(Email)
 )
